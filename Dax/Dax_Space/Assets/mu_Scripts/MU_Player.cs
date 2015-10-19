e9c45a88386a9cd168633e9c2eb99a9ab8353e59
Horrorbuns <limeside@gmail.com>
2015-10-19 15:52:54 -0500

    Dax Prototype Added
    Added prototype project.

---
+++ Dax/Dax_Space/Assets/mu_Scripts/MU_Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MU_Player : MonoBehaviour {
+
+	public Rigidbody2D mu_rigid;
+	
+	public float mu_moveSpeed;
+	
+	// Use this for initialization
+	void Start () {
+		mu_rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MuPlayerControls();
+	
+	}
+	
+	public void MuPlayerControls(){
+		//Left
+		if (Input.GetKey(KeyCode.A)){
+			mu_rigid.AddForce(new Vector2(-mu_moveSpeed,0f),ForceMode2D.Force);			
+		}
+		//Right
+		if (Input.GetKey (KeyCode.D)){
+			mu_rigid.AddForce(new Vector2(mu_moveSpeed,0f),ForceMode2D.Force);
+		}
+		//Up
+		if (Input.GetKey (KeyCode.W)){
+			mu_rigid.AddForce(new Vector2(0f,mu_moveSpeed),ForceMode2D.Force);
+		}
+		
+		//Down
+		if (Input.GetKey (KeyCode.S)){
+			mu_rigid.AddForce(new Vector2(0f,-mu_moveSpeed),ForceMode2D.Force);
+		}
+		//Space
+	}
+}
