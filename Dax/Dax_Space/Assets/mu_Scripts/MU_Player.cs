92e4ca9fdb8684dc6d661af23bed1b2ae485e7dc
Horrorbuns <limeside@gmail.com>
2015-10-20 18:59:38 -0500

    Boost Mechanic
    Added boost mechanic and related objects.

--- Dax/Dax_Space/Assets/mu_Scripts/MU_Player.cs
+++ Dax/Dax_Space/Assets/mu_Scripts/MU_Player.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MU_Player : MonoBehaviour {
 
 	public Rigidbody2D mu_rigid;
+	public Text mu_text;
 	
 	public float mu_moveSpeed;
+	public float mu_boostCount = 0;
+	
+	private Slider mu_boostSlider;
 	
 	// Use this for initialization
 	void Start () {
 		mu_rigid = GetComponent<Rigidbody2D>();
+		mu_boostSlider = FindObjectOfType<Slider>().GetComponent<Slider>();
 	}
 	
 	// Update is called once per frame
@@ -41,7 +47,8 @@ public class MU_Player : MonoBehaviour {
 		if (Input.GetKey (KeyCode.S)){
 			mu_rigid.AddForce(new Vector2(0f,-mu_moveSpeed),ForceMode2D.Force);
 		}
-		//Space
+		
+		MU_SpeedBoost();
 		
 		//Limit the Y movement of the player to certain parameters.
 		//Move to separate function later?
@@ -49,4 +56,30 @@ public class MU_Player : MonoBehaviour {
 		mu_playerTransform.y = Mathf.Clamp (mu_playerTransform.y,-2,4);
 		transform.position = mu_playerTransform;
 	}
+	
+	//Spacebar boost mechanic.
+	public void MU_SpeedBoost(){
+		mu_boostSlider.value = mu_boostCount;
+		float mu_boostText = Mathf.Ceil(mu_boostCount);
+		if (Input.GetKey (KeyCode.Space)){
+			mu_text.text = mu_boostText.ToString ("0.00");
+			mu_boostCount += .5f * Time.deltaTime;
+			if (Input.GetKeyUp(KeyCode.Space) && (mu_moveSpeed > .25 && mu_moveSpeed <.75)){
+				mu_moveSpeed *= 2;
+			} else if (Input.GetKeyUp (KeyCode.Space) && (mu_moveSpeed < .25 && mu_moveSpeed >.75)){
+				mu_moveSpeed /= 2;
+			} else {
+				return;
+			}
+		} else {
+			mu_boostCount = 0;
+		}
+		
+		mu_boostCount = 0;
+	}
+	
+	public float MU_BoostValue(){
+		return mu_boostCount;
+	}
+	
 }
