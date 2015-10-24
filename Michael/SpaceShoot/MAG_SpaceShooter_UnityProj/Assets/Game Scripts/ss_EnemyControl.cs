c1e05c746e17679cc34e578fa3a59606cf1b968b
Michael Anthony Gonzalez <gonzalez.a.michael@gmail.com>
2015-10-24 02:04:36 -0500

    Update 5
    added to some c# programming for Space shooter.
    added player and enemy control and spawn control.
    Programmed to feel like a top-down scrolling,
    I will adjust the programming to make it feel more like a side scroller.

---
+++ Michael/SpaceShoot/MAG_SpaceShooter_UnityProj/Assets/Game Scripts/ss_EnemyControl.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Michael Anthony Gonzalez
+ * Houston Community College
+ * Fall 2015
+ * MonoDevelop - Unity 5.1.2f1(64-bit))
+This class will be an Added Component to Enemy GameObject for programming Player;'s control 
+movement with the keyboard.
+ */
+using UnityEngine;
+using System.Collections;
+
+public class ss_EnemyControl : MonoBehaviour {
+
+	float ss_speed; //for the enemy speed.
+
+	// Use this for initialization
+	void Start () {
+	
+		ss_speed = 2f; // set speed
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//Get the enemy current position
+		Vector2 position = transform.position;
+
+		//Compute the enemy new postion.
+		position = new Vector2 (position.x, position.y - ss_speed * Time.deltaTime);
+
+		//Update the enemy postion.
+		transform.position = position;
+
+		//This is the bottom-left point of the screen.
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+		//If the enemy went outside the screen  on the bottom, then destroy the enemy.
+		if(transform.position.y < min.y)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
